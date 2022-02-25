0950d9d192360ef0d3aec1544ad880551d2e78aa
Shashi Bala <Avinash.verma@africa.airtel.com>
2022-02-25 19:08:04 +0530

    First

---
+++ src/test/java/com/shashi/wirelesscardemo/WirelesscardemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shashi.wirelesscardemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WirelesscardemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
